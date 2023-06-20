74a7aea3f4a5a6bd7b2f14ae5d3b3b675d12e74f
suupia <morita777k@icloud.com>
2023-06-21 00:39:52 +0900

    Amplifyに上限を追加

--- Assets/Projects/Fruit/Scripts/DoubleAmplify.cs
+++ Assets/Projects/Fruit/Scripts/DoubleAmplify.cs
@@ -14,11 +14,15 @@ namespace Projects.Fruit.Scripts
 {
     public class DoubleAmplify : IAmplify
     {
+        readonly int _maxAmplifyCount = 2;
         readonly float _amplifyOffset = 0.1f;
         readonly float _amplifyForce = 1f; 
         
         readonly FruitControllerBuilder _fruitControllerBuilder;
-         FruitType _fruitType;
+        
+       readonly   FruitType _fruitType;
+
+       int _currentAmplifyCount;
          
         public DoubleAmplify( FruitType fruitType, FruitControllerBuilder fruitControllerBuilder)
         {
@@ -28,11 +32,14 @@ namespace Projects.Fruit.Scripts
         }
         public void Amplify(Transform transform)
         {
+            if(_currentAmplifyCount >= _maxAmplifyCount) return;
             var offset = _amplifyOffset * ProjectUtility.RandomDownVector2();
             var fruitController =  _fruitControllerBuilder.Build(_fruitType,transform.position + (Vector3)offset);
             var fruitRb = fruitController.GetComponent<Rigidbody2D>();
             fruitRb.velocity = fruitController.Velocity;
             fruitRb.AddForce(_amplifyForce * ProjectUtility.RandomVector2(), ForceMode2D.Impulse);
+            
+            _currentAmplifyCount++;
         }
     }
 }
