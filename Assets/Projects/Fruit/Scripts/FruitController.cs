68d80ecad039dfafee1dde0b2642161803a4af4b
suupia <morita777k@icloud.com>
2023-06-20 11:12:40 +0900

    U_Rs: FruitsのPixelPerUnitを16に設定

--- Assets/Projects/Fruit/Scripts/FruitController.cs
+++ Assets/Projects/Fruit/Scripts/FruitController.cs
@@ -8,6 +8,8 @@ using Unity.Jobs.LowLevel.Unsafe;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
+using UniRx;
+using UniRx.Triggers;
 
 namespace Projects.Fruit.Scripts
 {
@@ -15,12 +17,18 @@ namespace Projects.Fruit.Scripts
     {
         public IFruit Fruit => _fruit;
         
+       readonly  float _destroyHeight = -10f;
+        
         IFruit _fruit;
         
         [Inject]
         public void Construct(IFruit fruit)
         {
             _fruit = fruit;
+
+            this.UpdateAsObservable()
+                .Where(_ => transform.position.y <= _destroyHeight)
+                .Subscribe(_ => Destroy(gameObject));
         }
         
     }
