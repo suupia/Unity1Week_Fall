75d3cc3c10959246bc3e0d258c9d37bb592d7a68
suupia <morita777k@icloud.com>
2023-06-20 21:21:49 +0900

    A_Sc: BounceOff()の実装

--- Assets/Projects/Fruit/Scripts/FruitController.cs
+++ Assets/Projects/Fruit/Scripts/FruitController.cs
@@ -13,21 +13,29 @@ using UniRx.Triggers;
 
 namespace Projects.Fruit.Scripts
 {
-    public class FruitController: MonoBehaviour
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class FruitController : MonoBehaviour
     {
         public IFruit Fruit => _fruit;
-        
-       readonly  float _destroyHeight = -10f;
-        
+
+        readonly float _bottomDestroyLine = -10f;
+        readonly float _topDestroyLine = 7f;
+
         IFruit _fruit;
-        
+
         public void Init(IFruit fruit)
         {
             _fruit = fruit;
+
+            // 低すぎる場所に行くとDestroyする
             this.UpdateAsObservable()
-                .Where(_ => transform.position.y <= _destroyHeight)
+                .Where(_ => transform.position.y <= _bottomDestroyLine)
+                .Subscribe(_ => Destroy(gameObject));
+
+            // 高すぎる場所に行くとDestroyする
+            this.UpdateAsObservable()
+                .Where(_ => transform.position.y >= _topDestroyLine)
                 .Subscribe(_ => Destroy(gameObject));
         }
-
     }
 }
--- Assets/Projects/Player/Scripts/PaddleController.cs
+++ Assets/Projects/Player/Scripts/PaddleController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Projects.Stage.Scripts;
@@ -7,18 +8,21 @@ using Projects.GameSystem.Interfaces;
 using VContainer;
 using UniRx;
 using UniRx.Triggers;
+using Projects.Fruit.Scripts;
 
 namespace Projects.Player.Scripts
 {
     public class PaddleController : MonoBehaviour
     {
-        private float speed = 0.018f;
+        float speed = 0.018f;
+        float forceAmount = 30f;
 
         [SerializeField] GameObject paddleObj; // widthを取るために必要 localScale.xで取得している
 
         IGameState _gameState;
         StageManager _stageManager;
 
+        // Constructでインジェクトできるのは、最初からシーン上にあるため
         [Inject]
         public void Construct(IGameState gameState, StageManager stageManager)
         {
@@ -32,14 +36,12 @@ namespace Projects.Player.Scripts
                 .Subscribe(Move)
                 .AddTo(this);
         }
-        
 
-        private void Move(float hInput)
+        void Move(float hInput)
         {
             transform.position += new Vector3(hInput * speed, 0);
         }
-
-        private bool CanMove(float hInput)
+        bool CanMove(float hInput)
         {
             if (hInput > 0 && transform.position.x >= _stageManager.StageWidth / 2.0f -
                 paddleObj.transform.localScale.x / 2.0f - _stageManager.HorizontalMargin)
@@ -56,6 +58,21 @@ namespace Projects.Player.Scripts
             {
                 return true;
             }
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.GetComponent<FruitController>() is { } fruitController)
+            {
+                BounceOff(fruitController);
+            }
+        }
+
+        void BounceOff(FruitController fruitController)
+        {
+            Debug.Log("Fruitを飛ばします");
+            var rb = fruitController.GetComponent<Rigidbody2D>();
+            rb.AddForce(forceAmount * Vector2.up, ForceMode2D.Impulse);
         }
     }
 }
