4bd4ec51537aa3389d2d0dfe1e5b9c7be8021b9c
suupia <morita777k@icloud.com>
2023-06-21 18:10:33 +0900

    FruitTypeSelectorを仮追加

---
+++ Assets/Projects/Fruit/Scripts/FruitTypeSelector.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Level.Interfaces;
+using Projects.Stage.Scripts;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+using UniRx;
+using UniRx.Triggers;
+using Object = UnityEngine.Object;
+using Random = Unity.Mathematics.Random;
+#nullable enable
+namespace Projects.Fruit.Scripts
+{
+    public class FruitTypeSelector
+    {
+        readonly ILevelManager _levelManager;
+        public FruitTypeSelector(ILevelManager levelManager)
+        {
+            _levelManager = levelManager;
+        } 
+        
+        public FruitType Select()
+        {
+            var random = UnityEngine.Random.Range(0, 1.0f);
+            return _levelManager.CurrentLevel switch
+            {
+                0 => LevelZero(random),
+                1 => LevelOne(random),
+            };
+        }
+
+        FruitType LevelZero(float random)
+        {
+            return random > 0.3f ? FruitType.Apple : FruitType.BadApple;
+        }
+        
+        FruitType LevelOne(float random)
+        {
+            return random > 0.5f ? FruitType.Apple : FruitType.BadApple;
+        }
+    }
+}
