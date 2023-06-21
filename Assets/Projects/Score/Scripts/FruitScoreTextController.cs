a208b91018781de55add3c79c16c05bd8f7fa094
suupia <morita777k@icloud.com>
2023-06-21 14:05:57 +0900

    FruitScoreのアニメーションを追加(再)

--- Assets/Projects/Score/Scripts/FruitScoreTextController.cs
+++ Assets/Projects/Score/Scripts/FruitScoreTextController.cs
@@ -1,31 +1,50 @@
 ﻿using TMPro;
 using UnityEngine;
+using DG.Tweening;
 
 namespace Projects.Score.Script
 {
-    
     public enum FruitScoreType
     {
         Positive,
         Negative
     }
+
     [RequireComponent(typeof(TextMeshPro))]
     public class FruitScoreTextController : MonoBehaviour
     {
         [SerializeField] Color positiveColor;
         [SerializeField] Color negativeColor;
 
+        readonly float _offsetHeight = 0.5f;
         readonly float _lifeTime = 1f;
-        
+        readonly float _moveDistance = 1f;
+
         public void Init(FruitScoreType type, string text)
         {
             Debug.Log($"Init FruitScoreTextController");
+            var pos = transform.position;
+            transform.position = new Vector2(pos.x, pos.y + _offsetHeight);
+
             var color = type == FruitScoreType.Positive ? positiveColor : negativeColor;
             var textMeshPro = GetComponent<TextMeshPro>();
-            textMeshPro.text = text;
-            textMeshPro.color = color;
-            Destroy(gameObject, _lifeTime);
+            var moveEndPos = transform.position.y + _moveDistance;
+
+
+            DOTween.Sequence()
+                .OnStart(() =>
+                {
+                    textMeshPro.text = text;
+                    textMeshPro.alpha = 1.0f;
+                    textMeshPro.color = color;
+                    textMeshPro.transform.localScale = Vector3.one * 0.3f;
+                })
+                .Append(textMeshPro.DOFade(0.0f, _lifeTime).SetEase(Ease.InCubic))
+                .Join(textMeshPro.transform.DOScale(1.0f, _lifeTime).SetEase(Ease.OutBack))
+                .Join(textMeshPro.transform.DOMoveY(moveEndPos + _moveDistance, _lifeTime)
+                    .SetEase(Ease.InFlash))
+                .OnComplete(() => { Destroy(gameObject); })
+                .Play();
         }
-        
     }
 }
