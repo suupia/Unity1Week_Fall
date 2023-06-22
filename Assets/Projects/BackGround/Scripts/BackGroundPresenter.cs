bb411713e06bddd6bc86c5f6bec9e60889ea6ddb
suupia <morita777k@icloud.com>
2023-06-22 13:13:29 +0900

    BackGroundの切り替えを実装

---
+++ Assets/Projects/BackGround/Scripts/BackGroundPresenter.cs
@@ -0,0 +1,28 @@
+﻿using Level.Interfaces;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UIElements;
+using VContainer;
+using Image = UnityEngine.UI.Image;
+
+namespace BackGround.Scripts
+{
+    public class BackGroundPresenter : MonoBehaviour
+    {
+        [SerializeField] Image backGroundImage;
+        [SerializeField] Sprite[] backGroundSprites;
+        
+        [Inject]
+        public void Construct(ILevelManager levelManager)
+        {
+            this.ObserveEveryValueChanged(_ => levelManager.CurrentLevel)
+                .Subscribe(level =>
+                {
+                    if (level < backGroundSprites.Length)
+                    {
+                        backGroundImage.sprite = backGroundSprites[level];
+                    }
+                });
+        }
+    }
+}
