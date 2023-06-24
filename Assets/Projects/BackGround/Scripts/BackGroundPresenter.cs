b079620e7d8c4dd57b2aee17562d3f9f9ef5739c
suupia <morita777k@icloud.com>
2023-06-25 07:27:15 +0900

    LevelUpのトランジションを実装

--- Assets/Projects/BackGround/Scripts/BackGroundPresenter.cs
+++ Assets/Projects/BackGround/Scripts/BackGroundPresenter.cs
@@ -1,4 +1,5 @@
-﻿using Level.Interfaces;
+﻿using DG.Tweening;
+using Level.Interfaces;
 using UniRx;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -9,9 +10,12 @@ namespace BackGround.Scripts
 {
     public class BackGroundPresenter : MonoBehaviour
     {
-        [SerializeField] Image backGroundImage;
-        [SerializeField] Sprite[] backGroundSprites;
+        [SerializeField]  Image backGroundImage;
+        [SerializeField] Image backGroundTransitionImage; // 切り替わる時のフェードイン用
+        [SerializeField]  Sprite[] backGroundSprites;
+        [SerializeField]  float transitionDuration = 1f;
         
+
         [Inject]
         public void Construct(ILevelManager levelManager)
         {
@@ -20,9 +24,27 @@ namespace BackGround.Scripts
                 {
                     if (level < backGroundSprites.Length)
                     {
-                        backGroundImage.sprite = backGroundSprites[level];
+                        DOTween.Sequence()
+                            .OnStart(() =>
+                            {
+                                // transitionImageのspriteを次の者にする
+                                backGroundTransitionImage.sprite = backGroundSprites[level];
+                                // 透明度を0にする
+                                var color = backGroundTransitionImage.color;
+                                backGroundTransitionImage.color = new Color(color.r, color.g, color.b, 0);
+                            })
+                            .Append(backGroundTransitionImage.DOFade(1f, transitionDuration).SetEase(Ease.InCubic))
+                            .Append(backGroundImage.DOFade(0f, transitionDuration).SetEase(Ease.OutCubic))
+                            .OnComplete(() =>
+                            {
+                                backGroundImage.sprite = backGroundSprites[level];
+                                backGroundImage.color = new Color(1, 1, 1, 1);
+                                backGroundTransitionImage.color = new Color(1, 1, 1, 0);
+                            })
+                            .Play();
                     }
                 });
         }
+        
     }
 }
