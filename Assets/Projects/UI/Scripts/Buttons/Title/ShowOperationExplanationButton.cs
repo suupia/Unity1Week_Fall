17e74044f94c9aa9e90bee97c12edd66d4f43a23
suupia <morita777k@icloud.com>
2023-06-25 15:26:31 +0900

    StartButtonの実装

---
+++ Assets/Projects/UI/Scripts/Buttons/Title/ShowOperationExplanationButton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+namespace Projects.UI.Buttons.Scripts
+{
+    public class ShowOperationExplanationButton : ButtonAnimationOnRollOver, IPointerClickHandler
+    {
+        [SerializeField] GameObject titleCanvas;
+        [SerializeField] GameObject optionExplanationCanvas;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            titleCanvas.SetActive(false);
+            optionExplanationCanvas.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Projects/UI/Scripts/Buttons/Title/StartGameButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+namespace Projects.UI.Buttons.Scripts
+{
+    public class StartGameButton: ButtonAnimationOnRollOver, IPointerClickHandler
+    {
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+    }
+}
